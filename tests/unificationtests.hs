cc586a944589e1e54921c6688e4c52f96594cb4d
Inga Baumgartner <inga.baumgartner@gmail.com>
2019-08-07 17:12:49 +0200

    add additional testcases for unification

--- tests/unificationtests.hs
+++ tests/unificationtests.hs
@@ -39,12 +39,39 @@ multipleEquationsInWhichOneFails =
       (unification [(V testMetaX, V testMetaY), (V testConcreteX, V testConcreteY)])
   )
 
+emptyProblem =
+  TestCase (
+    assertEqual
+      "An empty problem should return an empty solution."
+      [[]]
+      (unification [])
+  )
+
+twoBrachchesBothValid =
+  TestCase (
+    assertEqual
+      "A problem with two valid solutions should return both."
+      [[(testMetaY, testMetaZ), (testMetaX, testMetaZ)], [(testMetaX, testMetaZ), (testMetaY, testMetaX)]]
+      (unification [(BL [B testMetaX testMetaY, B testMetaY testMetaZ], BL [B testMetaX testMetaX, B testMetaZ testMetaX])])
+  )
+
+twoBrachchesOneInvalid =
+  TestCase (
+    assertEqual
+      "This test covers the removing of invaled branches."
+      [[(testMetaX, testConcreteY), (testMetaY, testMetaX), (testMetaZ, testConcreteX)]]
+      (unification [(BL [B testConcreteX testMetaY, B testMetaY testMetaZ], BL [B testConcreteY testMetaZ, B testMetaZ testMetaX])])
+  )
+
 testsUnification =
   TestList [
     TestLabel "unification_test1" twoIndependentEquations
     , TestLabel "unification_test2" twoDependentEquations
     , TestLabel "unification_test3" cyclicalEquations
     , TestLabel "unification_test4" multipleEquationsInWhichOneFails
+    , TestLabel "unification_test5" emptyProblem
+    , TestLabel "unification_test6" twoBrachchesBothValid
+    , TestLabel "unification_test7" twoBrachchesOneInvalid
   ]
 
 main = do
